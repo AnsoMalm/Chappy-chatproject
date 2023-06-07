34d28e1a8cc554673d20295a71023f5c5e9b01bc
AnsoMalm <annsophie.malmqvist85@hotmail.com>
2023-06-07 11:44:23 +0200

    added new structor for the project and put in data i db.json

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,19 @@
 import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+const app = express()
+dotenv.config()
+const port = process.env.PORT || 1325
+
+app.use( cors())
+app.use( express.json() )
+app.use((req, res, next) => {
+	console.log(`${req.method} ${req.url},`, req.body)
+	next()
+})
+
+
+app.listen(port, () => {
+	console.log(`Server is listening on port ${port}...`)
+})
